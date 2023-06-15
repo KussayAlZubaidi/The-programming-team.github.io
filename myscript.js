e4ee8abf24cd04c8dda3d9d027c3f5d094784d4e
jeffvarghese5 <47558882+Amberside@users.noreply.github.com>
2023-06-15 14:42:13 +1000

    a new folder with 10000000 has been added

--- myscript.js
+++ myscript.js
@@ -84,6 +84,11 @@ var students =
       "path": "100800000",
       "lastName": "FName",
       "firstName": "LName"
+    },
+    {
+      "path": "10000000",
+      "lastName": "Kussay ",
+      "firstName": "Again"
     }
 
     
